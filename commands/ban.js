bbd04d85b59315256ca12c796f8a7b5cfc7a1c1e
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-10-25 18:27:00 +0200

    One commit

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,17 @@
 
+{const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'NjMyMjkxODAwNTg1MDc2NzYx.XaNAoQ.MpR8EcnF4IhKI_wk6-o8gFv9uDw';
+
+const cheerio = require('cheerio');
+const request = require('request');
+const snekfetch = require('snekfetch');
+const ms = require('ms');
+const PREFIX = '>';
+var version = '1.0.2';
+
+
+const fs = require ('fs');}
 module.exports = {
     name: 'ban', 
     description: "bans a player",
