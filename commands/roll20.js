9f8e7248495bc12db87f6a90edd048e96a4fb30d
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-10-30 13:37:45 +0100

    Fixed an issue with roll20 command

--- commands/roll20.js
+++ commands/roll20.js
@@ -15,14 +15,15 @@ module.exports = {
     name: 'roll20', 
     description: "rolls a 20 sided dice",
     execute(message, agrs){
-            var facts = ["lfmaaao u got 1", "F you got 2", "rip thats a 3", "smh u got 4", "5 xd", "muhahah thats a 6", "F in the chat for u 7", "F pressed :v 8", "lmaaao a 9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19 :p", "whao how lucky 20"];
-            var fact = Math.floor(Math.random() * facts.length);
-            const rollEmbed = new Discord.RichEmbed()
-            .setTitle('You rolled a 20 sided dice and got...')
-            .addField((facts[fact]), 'Yeee haaaaw >:D')
-            .setThumbnail('https://media.giphy.com/media/RlqEFaA0gNSM1TgPok/giphy.gif')
-            .setColor('0x#1BEE1A')
-            .setFooter('Cowboish bot');
-            message.channel.sendEmbed(rollEmbed);
+        var facts = ["1", "2", "3", "4", "5 ", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
+        var fact = Math.floor(Math.random() * facts.length);
+        const rollEmbed = new RichEmbed()
+        .setTitle('You rolled a 20 sided dice and got...')
+        .addField((facts[fact]), 'Yeee haaaaw >:D')
+        .attachFiles (["./dice/" + "dice20.gif"])
+        .setImage('attachment://dice20.gif')
+        .setColor('0x#1BEE1A')
+        .setFooter('Cowboish bot');
+        message.channel.sendEmbed(rollEmbed);
     }
 }
