b588ac8bf77c1e549b4cccdd22b76005844aec3f
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-03 13:09:49 +0100

    edited the identify command

--- commands/identify.js
+++ commands/identify.js
@@ -15,13 +15,32 @@ module.exports = {
     name: 'identify', 
     description: "identifies what idv player ur",
     execute(message, agrs){
-var facts = ["Is the coordinator ho misses the flare gun xD", "Is the gardener who says 'Foucus on decoding' while dismantling a rocket chair...", "is the wildning who decodes the whole match, dude u better be a minds eye", "is the prospector who removes the hunter's attack recovery animation after getting hit...",
-"is this the guy that can't kite for sh*t?", "is the guy who drops the pallet down infront of his teammates face... what a betrayl", "is the player that begs for gifts in the server chat", "Camp = report", "Man stop getting terror shocked!", "the 'i was laggy' excuse user", "Disconnects early game and gets found first... GG",
-"Surrenders...", "WHY DO I GET FOUND FIRST!!", "The guy who says switch character when ur playing as a lawyer", "The guy who only talks sh*t in server chat", "The guy who thinks that idv is a dating game, it's not Tinder V bro", "Pls hunter be friendly i luv u <3", "Toxics at the exit gate and gets dragged by the gamekeepers hook..",
-"The 'Don't move i'm coming' mindseye, gg", "Omg these randoms are soo baaad",
-, "The 'im a hero' cowboi"];
-var fact = Math.floor(Math.random() * facts.length);
-message.channel.send(facts[fact]);
+        {if (!args[1]) return message.reply('Who do you want to identify? tag them in your 2nd args!')}
+        let persona  = message.mentions.users.first()
+
+        var facts = [
+        persona.username + " Is the coordinator who misses her flare gun",
+        persona.username +" Is the gardener who says 'Foucus on decoding' while dismantling a rocket chair...", 
+        persona.username + " is the wilding who decodes the whole match, dude u better be a minds eye", 
+        persona.username + " is the prospector who removes the hunter's attack recovery animation after getting hit, good job!",
+        persona.username +" is this the guy that can't kite for sh*t?", 
+        persona.username + " is the guy who drops the pallet down infront of his teammates face... what a betrayl",
+        persona.username + " is the player that begs for gifts in the server chat",
+        persona.username + " is the player who sends Camp = report", 
+        'Hey!' +  persona.username + " plz stop getting terror shocked!",
+        persona.username + " is the 'i was laggy' excuse user", 
+        persona.username + " always disconnects early game and gets found first... GG",
+        persona.username + " always Surrenders...", 
+        persona.username + ": WHY DO I GET FOUND FIRST!!", 
+        persona.username + " is guy who only talks sh*t in server chat", 
+        persona.username + " is the guy who thinks that idv is a dating game, it's not Tinder V bro", 
+        "Pls hunter be friendly i luv u <3 thats what " + persona.username + ' usually says' ,
+        persona.username + " toxics at the exit gate and gets dragged by the gamekeepers hook..",
+        persona.username + " is the 'Don't move i'm coming' mindseye, gg", 
+        "Omg these randoms are soo baaad, that's what " + persona.username + ' said.',
+        persona.username + " thinks he is the hero cowboi, heh"];
+        var fact = Math.floor(Math.random() * facts.length);
+        message.channel.send(facts[fact]);
 
 
 
