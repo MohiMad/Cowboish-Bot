1bad0553defbb3b63919714faf5cec03a730e07c
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-07 16:45:38 +0100

    Added a p5 essence command

--- commands/essence.js
+++ commands/essence.js
@@ -15,16 +15,45 @@ module.exports = {
     name: 'essence', 
     description: "opens an essence",
     execute(message, args){
-            number = 45;
-            imagNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-            const newEmbed = new RichEmbed()
-            .setTitle('You opened an essence [S8 No.2] and got...')
-            .attachFiles (["./s8/" + 'essence' + imagNumber + ".PNG"])
-            .setURL('https://rkanjo2.wixsite.com/cowboishbot')
-            .setImage('attachment://essence' + imagNumber + '.PNG')
-            .setColor('0x#1BEE1A')
-            .setFooter('Cowboish bot');
-            message.channel.send(newEmbed);
+
+        
+        number = 45;
+        imagNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+        const newEmbed = new RichEmbed()
+        .setTitle('You opened an essence [S8 No.2] and got...')
+        .attachFiles (["./s8/" + 'essence' + imagNumber + ".PNG"])
+        .setURL('https://rkanjo2.wixsite.com/cowboishbot')
+        .setImage('attachment://essence' + imagNumber + '.PNG')
+        .setColor('0x#1BEE1A')
+        .setFooter('NOTE: This command doesnt have the same ingame percentage');
+
+        numba = 32;
+        imageNumba = Math.floor (Math.random() * (numba - 1 + 1)) + 1;
+        const p5Embed = new RichEmbed()
+        .setTitle('You opened an essence [P5 No.2] and got...')
+        .attachFiles (["./p5/" + 'p5-' + imageNumba + ".jpg"])
+        .setURL('https://rkanjo2.wixsite.com/cowboishbot')
+        .setImage('attachment://p5-' + imageNumba + '.jpg')
+        .setColor('0x#1BEE1A')
+        .setFooter('NOTE: This command doesnt have the same ingame percentage');
+    
+        {if (!args[1]) message.channel.send('What essence do you want to open? please choose between ``p5`` or ``s8``');}
+
+        {if ((args[1]) == 'p5')
+        message.channel.sendEmbed(p5Embed);}
+
+        {if ((args[1]) == 's8')
+        message.channel.sendEmbed(newEmbed);}
+
+
+
+
+
+
+
+
+
+
     
 
 
