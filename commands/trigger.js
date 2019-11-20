2e170a9b164af71cf127cfa4a1055a0a3e3ef8a1
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-20 19:05:27 +0100

    Create trigger.js

---
+++ commands/trigger.js
@@ -0,0 +1,31 @@
+
+const token = 'NjMyMjkxODAwNTg1MDc2NzYx.XaNAoQ.MpR8EcnF4IhKI_wk6-o8gFv9uDw';
+const Discord = require('discord.js');
+const snekfetch = require ('snekfetch');
+module.exports = {
+    name: 'trigger',
+    description: "triggers you n ur friends",
+execute : async (message, args) => {
+
+  let target = message.mentions.users.first() || message.guild.members.get(args[0]).user || message.author;
+    let profilepic = target.avatarURL;
+    let url = `https://arcadia-api.xyz/api/v1/triggered?url=${profilepic}`;
+
+    message.channel.startTyping();
+
+    snekfetch.get(url, {
+        headers: {
+            "Authorization": token
+        }
+    }).then(async res => {
+        await message.channel.send({
+           files: [{
+               attachment: res.body,
+               name: `${target.tag}-triggered.gif`
+           }]
+        }).then(() => message.channel.stopTyping());
+    }).catch(err => console.error(err));
+
+}
+
+}
