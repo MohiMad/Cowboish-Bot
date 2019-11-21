0077f8b4277967f8953050439fd9fc9bac5ad680
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-21 11:09:23 +0100

    Update idv.js

--- commands/idv.js
+++ commands/idv.js
@@ -28,7 +28,7 @@ module.exports = {
 
             let memeNumComments = content[0].data.children[0].data.num_comments;
 
-            let txt = memeImage || joke || memeImage + joke;
+            let txt = memeImage || joke;
 
             const embeed = new RichEmbed()
             .setTitle(`${memeTitle}`)
