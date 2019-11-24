256b8eafe41bb11537c6c5a8ec6a9088a6f6714f
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-24 14:00:07 +0100

    Edited sum in index.js added blink command

---
+++ commands/blink.js
@@ -0,0 +1,37 @@
+const {Client, RichEmbed} = require('discord.js');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'blink',
+    description: "blink yo azz",
+    execute(message, args){
+        if (!args[1]) return message.reply('Who do you want to blinj? Mention them right after the command | example: >blink @Cowboish Bot. heh try to blink me >:D').then(m => m.delete(10000));
+
+        
+        let persona  = message.mentions.users.first()
+
+        let person = message.author.username
+
+        nuber = 6;
+
+        imagaNumber = Math.floor (Math.random() * (nuber - 1 + 1)) + 1;
+
+        var facts = ["Oof " + persona + " got their ass blinked by " + person, 
+        person + " used their blink on " + persona + " nice blink tbh"];
+
+        var fact = Math.floor(Math.random() * facts.length);
+
+        const lassoembed = new RichEmbed()
+        .setAuthor((facts[fact]), message.author.avatarURL)
+        .attachFiles (["./blink/" + 'blink' + imagaNumber + ".gif"])
+        .setImage('attachment://blink' + imagaNumber + '.gif')
+        .setColor("RANDOM")
+        .setFooter('Want your replay to pop up here? add me to submit them => (MohiMeaww#2888)');
+        
+        if(message.mentions.users.first().id === message.author.id) 
+            return message.channel.send ("Nah don't waste you blink on yourself, " + message.author.username);
+
+        else message.channel.sendEmbed(lassoembed);
+
+    }
+}
--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const request = require('request');
 const snekfetch = require('snekfetch');
 const ms = require('ms');
 var version = '1.0.2';
-const fs = require ('fs');
+const fs = require('fs');
 bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(File => File.endsWith('.js'));
-for (const file of commandFiles){
-const command = require(`./commands/${file}`);
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
 
 
 
-bot.commands.set(command.name, command);
+    bot.commands.set(command.name, command);
 
 
-var time = new Date();
-var timestamp = '[' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ']';
+    var time = new Date();
+    var timestamp = '[' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds() + ']';
 
 
 }
@@ -39,18 +39,18 @@ bot.on("guildCreate", guild => {
     bot.on("guildDelete", guild => {
         console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
     });
-    
+
 
     const welcomeEmbed = new Discord.RichEmbed()
-    .setTitle('Thank you for inviting me to the party ;D')
-    .addField('My cowboish bithday 🎉🎊 14/10/2019', 'use >help and i will be there for help :)')
-    .attachFiles (["./emoji" + ".png"])
-    .setThumbnail('attachment://emoji' + '.png')
-    .setColor("RANDOM")
-    .addField('My prefix is ">"', 'Remember using it before any command')
-    .addField(`Now i'm in **${bot.guilds.size}** servers :)`, 'And growing <3')
-    .addField("Support me ♡ ♥", "[Cowboish website](https://rkanjo2.wixsite.com/cowboishbot)" +  " | [Invite me to servers around ;D](https://discordapp.com/oauth2/authorize?client_id=632291800585076761&scope=bot&permissions=8)");
-    
+        .setTitle('Thank you for inviting me to the party ;D')
+        .addField('My cowboish bithday 🎉🎊 14/10/2019', 'use >help and i will be there for help :)')
+        .attachFiles(["./emoji" + ".png"])
+        .setThumbnail('attachment://emoji' + '.png')
+        .setColor("RANDOM")
+        .addField('My prefix is ">"', 'Remember using it before any command')
+        .addField(`Now i'm in **${bot.guilds.size}** servers :)`, 'And growing <3')
+        .addField("Support me ♡ ♥", "[Cowboish website](https://rkanjo2.wixsite.com/cowboishbot)" + " | [Invite me to servers around ;D](https://discordapp.com/oauth2/authorize?client_id=632291800585076761&scope=bot&permissions=8)");
+
     let channel = bot.channels.get(guild.systemChannelID || channelID);
     channel.send(welcomeEmbed);
 });
@@ -64,17 +64,17 @@ bot.on('ready', () => {
 
     bot.user.setActivity('Identit | >help')
     const activities_list = [
-        " Use >help to send help", 
-        "Identit | >help", 
+        " Use >help to send help",
+        "Identit | >help",
         "Identity V | >help",
-    "Welcome to Identit | >help",
-    "Identit | >help", `idv in ${bot.guilds.size} servers`
-        ];
-    
-        setInterval(() => {
-            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-            bot.user.setActivity(activities_list[index]); 
-        }, 100000);
+        "Welcome to Identit | >help",
+        "Identit | >help", `idv in ${bot.guilds.size} servers`
+    ];
+
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        bot.user.setActivity(activities_list[index]);
+    }, 100000);
 })
 
 
@@ -84,84 +84,17 @@ bot.on('message', async message => {
 
     let args = message.content.substring(prefix.length).split(" ");
 
-      if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
 
     switch (args[0]) {
 
+        //Identity V commands Starts Here!
         case "menurole":
             bot.commands.get('menurole').execute(message, args);
-                break;
-                
+            break;
+
         case "roll20":
             bot.commands.get('roll20').execute(message, args);
-                break;
-
-        case "joke":
-            bot.commands.get('joke').execute(message, args);
-                break;
-
-        case "yee":
-            bot.commands.get('yee').execute(message, args);
-            break;
-
-        case "help":
-            bot.commands.get('help').execute(message, args);
-            break;
-
-        case 'guilds':
-            message.channel.send(`Cowboish Bot is now in ${bot.guilds.size} servers :D`)
-        break;
-
-        case 'meme':
-            bot.commands.get('meme').execute(message, args);  
-        break;
-
-        case 'idv':
-            bot.commands.get('idv').execute(message, args);  
-        break;
-
-        case 'say':
-
-            const sayMessage = args.slice(1).join(" ");
-
-            message.delete().catch(O_o=>{}); 
-
-            if (!args[1]) 
-            message.channel.send("Tell me what to say...");
-
-            message.channel.send(sayMessage);
-        break;
-
-        case "stun":
-            bot.commands.get('stun').execute(message, args);
-                break; 
-
-        case "crash":
-            bot.commands.get('crash').execute(message, args);
-                break; 
-
-        case "terrorshock":
-            bot.commands.get('terrorshock').execute(message, args);
-        break;
-
-        case "info":
-            bot.commands.get('info').execute(message, args);
-            break;
-
-        case "lasso":
-            bot.commands.get('lasso').execute(message, args);
-        break;
-
-        case "shoot":
-                bot.commands.get('shoot').execute(message, args);
-                break;
-
-        case "bully":
-            bot.commands.get('bully').execute(message, args);
-        break;
-
-        case "clear":
-            bot.commands.get('clear').execute(message, args);
             break;
 
         case "identify":
@@ -170,41 +103,128 @@ bot.on('message', async message => {
 
         case "winrate":
             bot.commands.get('winrate').execute(message, args);
-                break;
+            break;
 
         case "essence":
             bot.commands.get('essence').execute(message, args);
-                break;
+            break;
 
         case "roll":
             bot.commands.get('roll').execute(message, args);
             break;
 
-            case "randomize":
-                bot.commands.get('random').execute(message, args);
-                break;
+        case "randomize":
+            bot.commands.get('random').execute(message, args);
+            break;
+
+        //End Of identity V commands
+
+
+
+        //Fun Commands Starts Here!
+        case "joke":
+            bot.commands.get('joke').execute(message, args);
+            break;
+
+        case "yee":
+            bot.commands.get('yee').execute(message, args);
+            break;
+
+        case 'meme':
+            bot.commands.get('meme').execute(message, args);
+            break;
+
+        case 'idv':
+            bot.commands.get('idv').execute(message, args);
+            break;
+
+        case 'say':
+
+            const sayMessage = args.slice(1).join(" ");
+
+            message.delete().catch(O_o => { });
+
+            if (!args[1])
+                message.channel.send("Tell me what to say...");
+
+            message.channel.send(sayMessage);
+            break;
+
+        //End Of Fun commands
+
+
+
+
+        //Identity V Actions 
+        case "blink":
+            bot.commands.get('blink').execute(message, args);
+            break;
+
+        case "stun":
+            bot.commands.get('stun').execute(message, args);
+            break;
+
+        case "crash":
+            bot.commands.get('crash').execute(message, args);
+            break;
+
+        case "terrorshock":
+            bot.commands.get('terrorshock').execute(message, args);
+            break;
+
+        case "lasso":
+            bot.commands.get('lasso').execute(message, args);
+            break;
+
+        case "shoot":
+            bot.commands.get('shoot').execute(message, args);
+            break;
+
+        case "bully":
+            bot.commands.get('bully').execute(message, args);
+            break;
+        //End of Identity V Actions
+
+
+
+        //Moderation Commands starts here
+        case "clear":
+            bot.commands.get('clear').execute(message, args);
+            break;
 
         case "kick":
             bot.commands.get('kick').execute(message, args);
             break;
-
 
         case "ban":
             bot.commands.get('ban').execute(message, args);
             break;
 
         case "mute":
-            
             bot.commands.get('mute').execute(message, args);
-                
-        break;
+            break;
+        //End of moderation commands
+
+
+        //Config commands starts here
+        case "info":
+            bot.commands.get('info').execute(message, args);
+            break;
 
         case 'ping':
             const m = await message.channel.send("Ping?");
             m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
             break;
-        
-           
+
+        case "help":
+            bot.commands.get('help').execute(message, args);
+            break;
+
+        case 'guilds':
+            message.channel.send(`Cowboish Bot is now in ${bot.guilds.size} servers :D`)
+            break;
+
+        //End of config commands
 
     }
 });
