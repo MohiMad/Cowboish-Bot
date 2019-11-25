fb120b50cdf883ee6333276bf061fd84ccc440d2
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-11-25 09:39:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NjMyMjkxODAwNTg1MDc2NzYx.XaNAoQ.MpR8EcnF4IhKI_wk6-o8gFv9uDw';
+const token = 'NjMyMjkxODAwNTg1MDc2NzYx.XduTDQ.EbNApplniB0v8cyWCDOxdtf2mD0';
 const cheerio = require('cheerio');
 const request = require('request');
 const snekfetch = require('snekfetch');
