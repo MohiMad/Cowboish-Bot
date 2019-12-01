a5be697e6f5f9a72d04bc8c08fb13b087bcc3b97
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-12-01 15:27:45 +0100

    Update identify.js

--- commands/identify.js
+++ commands/identify.js
@@ -7,58 +7,36 @@ module.exports = {
     description: "identifies ur friends",
     execute(message, args){
 
-        const persona = message.mentions.users.first()
+        let target = message.mentions.users.first() || message.author;
         
                     var infos = [
-                    message.author.username + " Is the coordinator who misses her flare gun",
-                    message.author.username +" Is the gardener who says 'Foucus on decoding' while dismantling a rocket chair...", 
-                    message.author.username + " is the wilding who decodes the whole match, dude u better be a minds eye", 
-                    message.author.username + " is the prospector who removes the hunter's attack recovery animation after getting hit, good job!",
-                    message.author.username +" is this the guy that can't kite for sht?", 
-                    message.author.username + " is the guy who drops the pallet down infront of his teammates face... what a betrayl",
-                    message.author.username + " is the player that begs for gifts in the server chat",
-                    message.author.username + " is the player who sends Camp = report", 
-                    'Hey!' +  message.author.username + " plz stop getting terror shocked!",
-                    message.author.username + " is the 'i was laggy' excuse user", 
-                    message.author.username + " always disconnects early game and gets found first... GG",
-                    message.author.username + " always Surrenders...", 
-                    message.author.username + ": WHY DO I GET FOUND FIRST!!", 
-                    message.author.username + " is guy who only talks sht in server chat", 
-                    message.author.username + " is the guy who thinks that idv is a dating game, it's not Tinder V bro", 
-                    "Pls hunter be friendly i luv u :heart: thats what " + message.author.username + ' usually says' ,
-                    message.author.username + " toxics at the exit gate and gets dragged by the gamekeepers hook..",
-                    message.author.username + " is the 'Don't move i'm coming' mindseye, gg", 
-                    "Omg these randoms are soo baaad, that's what " + message.author.username + ' said.',
-                    message.author.username + " thinks he is the hero cowboi, heh"];
+                    target.username + " Is the coordinator who misses her flare gun",
+
+                    target.username +` Is the gardener who says "Foucus on decoding" while dismantling a rocket chair...`, 
+
+                    target.username + " is the wilding who only decode the whole match, dude u better be a minds eye", 
+                    target.username + " is the prospector who removes the hunter's attack recovery animation after getting hit, good job!",
+                    target.username +" is this the guy who can't kite for shit?", 
+                    target.username + " is the guy who drops the pallet down infront of his teammates face... we aren't playing blackjack are we?",
+                    target.username + " is the player who begs for gifts in the server chat",
+                    target.username + ` is the player who sends "Camp = report" before the match starts, probably got camped on.`, 
+                    'Hey!' +  target.username + " plz stop getting terror shocked!",
+                    target.username + ` is the "i was laggy" excuse user`, 
+                    target.username + " always disconnects early game and gets found first... GG",
+                    target.username + ": tries to surrenders but unable to cuz the hunter downed the whole team too early", 
+                    target.username + ": WhY dO I gEt fOuND fIrST!", 
+                    target.username + " is guy who only talks shit in global chat", 
+                    target.username + " is the guy who thinks that idv is a dating game, it's not Tinder V bro", 
+                    "Pls hunter be friendly i luv u :heart: thats what " + target.username + ' usually says' ,
+                    target.username + " toxics at the exit gate and gets dragged by the gamekeepers hook..",
+                    target.username + ` is the "Don't move i'm coming" mindseye, gg`, 
+                    "Omg these randoms are soo baaad, stop complaining about randoms, " + target.username,
+                    target.username + " thinks he is the hero cowboi, heh"];
 
                     var info = Math.floor(Math.random() * infos.length);
 
-                    if (!args[1])  return message.channel.send(infos[info])
-
-                var facts = [
-                persona.username + " Is the coordinator who misses her flare gun",
-                persona.username +" Is the gardener who says 'Foucus on decoding' while dismantling a rocket chair...", 
-                persona.username + " is the wilding who decodes the whole match, dude u better be a minds eye", 
-                persona.username + " is the prospector who removes the hunter's attack recovery animation after getting hit, good job!",
-                persona.username +" is this the guy that can't kite for sh*t?", 
-                persona.username + " is the guy who drops the pallet down infront of his teammates face... what a betrayl",
-                persona.username + " is the player that begs for gifts in the server chat",
-                persona.username + " is the player who sends Camp = report", 
-                'Hey!' +  persona.username + " plz stop getting terror shocked!",
-                persona.username + " is the 'i was laggy' excuse user", 
-                persona.username + " always disconnects early game and gets found first... GG",
-                persona.username + " always Surrenders...", 
-                persona.username + ": WHY DO I GET FOUND FIRST!!", 
-                persona.username + " is guy who only talks sh*t in server chat", 
-                persona.username + " is the guy who thinks that idv is a dating game, it's not Tinder V bro", 
-                "Pls hunter be friendly i luv u <3 thats what " + persona.username + ' usually says' ,
-                persona.username + " toxics at the exit gate and gets dragged by the gamekeepers hook..",
-                persona.username + " is the 'Don't move i'm coming' mindseye, gg", 
-                "Omg these randoms are soo baaad, that's what " + persona.username + ' said.',
-                persona.username + " thinks he is the hero cowboi, heh"];
-                var fact = Math.floor(Math.random() * facts.length);
-
-                message.channel.send(facts[fact]);
+                    message.channel.send(info);
+                
 
     }
 }
