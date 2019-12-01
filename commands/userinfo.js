54fe0c5d777d33b4261592b3339e948828103296
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-12-01 21:09:44 +0100

    noice

---
+++ commands/userinfo.js
@@ -0,0 +1,47 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../functions.js");
+
+
+
+module.exports = {
+    name: 'userinfo', 
+    description: "show info",
+    execute(message, args){
+        
+
+        const member = getMember(message, args.join(" "));
+
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
+
+        // User variables
+        const created = formatDate(member.user.createdAt);
+
+        const embed = new RichEmbed()
+            .setFooter(member.displayName, member.user.displayAvatarURL)
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+
+            .addField('👇 User information 👇 ', stripIndents`**> Nickname **• ${member.displayName}
+            **> 📌 Joined server **• ${joined}
+            **> 👉 Roles **• ${roles}`, true)
+
+            .addField('👇 User information 👇', stripIndents`**> ID **• ${member.user.id}
+            **> 💬 Username**• ${member.user.username}
+            **> 📎 Tag**• ${member.user.tag}
+            **> 🎉 Account's birthday**• ${created}`, true)
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('🔴Currently playing', stripIndents`**>** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+
+
+
+    }
+}
--- index.js
+++ index.js
@@ -206,6 +206,10 @@ bot.on('message', async message => {
             bot.commands.get('info').execute(message, args);
             break;
 
+        case "userinfo":
+            bot.commands.get('userinfo').execute(message, args);
+        break;
+
         case 'ping':
             const m = await message.channel.send("Ping?");
             m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
