bae7dc468400275a76d0c9302fda5b5b5dad65dd
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-12-17 20:08:28 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -11,7 +11,9 @@ message.delete().catch(O_o => { });
 if (!args[1])
     message.channel.send("Tell me what to say...");
 
+else {
 message.channel.send(message.author.username + " says\n ``" + sayMessage + "``");
 
+}
     }
 }
