e0e730a75fa0b73c49eb43287b257229b623ad13
MohiMad <56826704+MohiMad@users.noreply.github.com>
2019-12-28 16:37:30 +0100

    Update logicpath.js

--- commands/logicpath.js
+++ commands/logicpath.js
@@ -59,7 +59,7 @@ module.exports = {
                     S: 0,
                     A: 0,
                     B: 0,
-                    C: 0,
+                    C: 0, 
                     D: 0
 
                 })
