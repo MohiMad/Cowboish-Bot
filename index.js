4324071eaa587decbda8cc8abed5ae620fd847b6
MohiMad <56826704+MohiMad@users.noreply.github.com>
2020-02-16 18:01:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-const serva = server.listen(process.env.PORT);
+const serva = server.listen(44002);
 
 const dbl_webhook = new DBL(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjI5MTgwMDU4NTA3Njc2MSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTc0NjAyNTIxfQ.0FNoMoV2BBfO7EdAcKkIDsX_N6CsHsjabC1kbzmbBNY", {
-    webhookAuth: 'mmkdmkmmkdmk',
-    webhookPort: process.env.PORT,
+    webhookAuth: 'Mohamad2004',
+    webhookPort: 44002,
     webhookServer: serva
 }, bot);
 
