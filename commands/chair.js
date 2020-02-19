e9a0bbcc1e45880f34eb292fb0c1b806202e4349
MohiMad <56826704+MohiMad@users.noreply.github.com>
2020-02-19 12:53:24 +0100

    Update chair.js

--- commands/chair.js
+++ commands/chair.js
@@ -8,30 +8,32 @@ module.exports = {
 
         const chaired = await findMember(message, args[1]) || message.author;
 
-        if(!chaired){
-            ErrorMsg(bot, message, "Couldn't find that member, please provide their ID, userTag or mention them after the command!\n\nUsage: `>chair <mentionHERE>`")
-        }else{
-        const canvas = Canvas.createCanvas(1200, 900);
-        const ctx = canvas.getContext('2d');
-        // Since the image takes time to load, you should await it
-        const background = await Canvas.loadImage('./pics/chair.jpg');
-        // This uses the canvas dimensions to stretch the image onto the entire canvas
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        // Use helpful Attachment class structure to process the file for you
+        if (args[1]) {
+            if (!chaired) {
+                ErrorMsg(bot, message, "Couldn't find that member, please provide their ID, userTag or mention them after the command!\n\nUsage: `>chair <mentionHERE>`")
+            }
+        } else {
+            const canvas = Canvas.createCanvas(1200, 900);
+            const ctx = canvas.getContext('2d');
+            // Since the image takes time to load, you should await it
+            const background = await Canvas.loadImage('./pics/chair.jpg');
+            // This uses the canvas dimensions to stretch the image onto the entire canvas
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+            // Use helpful Attachment class structure to process the file for you
 
-        ctx.strokeStyle = '#74037b';
-        // Draw a rectangle with the dimensions of the entire canvas
-        ctx.strokeRect(1, 1, canvas.width, canvas.height);
+            ctx.strokeStyle = '#74037b';
+            // Draw a rectangle with the dimensions of the entire canvas
+            ctx.strokeRect(1, 1, canvas.width, canvas.height);
 
-        const avatar = await Canvas.loadImage(chaired.user.displayAvatarURL);
-        // Move the image downwards vertically and constrain its height to 200, so it's a square
-        ctx.drawImage(avatar, 547, 500, 109, 109);
+            const avatar = await Canvas.loadImage(chaired.user.displayAvatarURL);
+            // Move the image downwards vertically and constrain its height to 200, so it's a square
+            ctx.drawImage(avatar, 547, 500, 109, 109);
 
- 
 
-        const attachment = new Discord.Attachment(canvas.toBuffer(), 'getChairedbich.gif');
 
-        message.channel.send(`<:rocketchair:679651828157513741> HAH get chaired **${chaired.user.username}**!`, attachment)
+            const attachment = new Discord.Attachment(canvas.toBuffer(), 'getChairedbich.gif');
+
+            message.channel.send(`<:rocketchair:679651828157513741> HAH get chaired **${chaired.user.username}**!`, attachment)
         }
     }
 }
