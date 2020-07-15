7657bb9fb6e75b375fb80576af0816a5a835abc8
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2020-07-15 14:53:47 +0200

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -61,7 +61,6 @@ module.exports = {
                 else if ([4, 5, 6, 7, 8, 9, 10, 11, 12, 13].includes(variable)) fragments = fragments + 200;
                 else fragments = fragments + 36;
             } else {
-
                 LP.Opened = [...LP.Opened, `${essence}-${variable}`];
 
                 if (variable === 0) LP.S = LP.S + 1;
@@ -69,6 +68,8 @@ module.exports = {
                 else if ([4, 5, 6, 7, 8, 9, 10, 11, 12, 13].includes(variable)) LP.B = LP.B + 1;
                 else if ([21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36].includes(variable)) LP.C = LP.C + 1;
                 else LP.D = LP.D + 1;
+
+                LP.save().catch(e => console.log(e));
             }
 
         }
