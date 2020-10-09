8d9463471172866a011688616186c3855b021567
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2020-10-09 22:58:15 +0200

    Tryna reduce the job in ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,5 @@
-const got = require("got");
-const config = require("../config.json");
-const snekfetch = require('snekfetch');
-const DBL = require("dblapi.js");
-const BOATS = require('boats.js');
-const schedule = require('node-schedule');
-const { rewards } = require("../functions.js");
-//const GBL = require('gblapi.js');
+
+
 
 const Mutes = require("../models/mutes");
 const Cooldown = require("../models/cooldown.js");
@@ -34,40 +28,7 @@ module.exports = async (bot) => {
         bot.user.setActivity(activities_list[index]);
     }, 300000);
 
-    schedule.scheduleJob("1 12 * * 1", async function () {
-        const checkForWeeklyExecution = await Cooldown.findOne({ userID: bot.user.id, command: "weeklyrewards" });
-
-        if (checkForWeeklyExecution) return;
-        const weeklySpamStopper = new Cooldown({
-            command: "weeklyrewards",
-            userID: bot.user.id,
-            timeRemaining: Date.now() + 300000,
-            dateNow: Date.now()
-        });
-
-        const dbl = new DBL(config.dbl_token, bot);
-
-        dbl.postStats(botGuildCount).catch(e => console.log(e));
-
-
-        const Boats = new BOATS(config.boatsToken);
-
-        Boats.postStats(botGuildCount, "632291800585076761")
-            .catch((err) => {
-                console.log(err);
-            });
-
-        await weeklySpamStopper.save().catch(err => console.log(err));
-        rewards(bot);
-    });
-
-    schedule.scheduleJob("0 9 * * *", async function () {
-        const checkForExecution = await Cooldown.findOne({ userID: bot.user.id, command: "giveaway" });
-
-        if (checkForExecution) return;
-        await bot.commands.get('giveaway').execute(bot);
-
-    });
+    
 
     setInterval(async () => {
 
@@ -111,23 +72,6 @@ module.exports = async (bot) => {
         }
     }, 3000);
 
-    /*const Glenn = new GBL(bot.user.id, config.glenToken, false, false);
-
-    Glenn.updateStats(botGuildCount).catch(e => console.log(e));
-
-    const updateBotList = async () => {
-
-        const { body: reply } = await snekfetch.post(`https://discordbotlist.com/api/bots/632291800585076761/stats`)
-            .set("Authorization", `Bot ${config.dblToken_2}`)
-            .send({
-                guilds: botGuildCount,
-                users: bot.users.size,
-            })
-
-        return (reply);
-    }
-
-    let botUPDATE = await updateBotList();
-    */
+    
 
 };
--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const bot = new Discord.Client({
     messageCacheMaxSize: 1,
     fetchAllMembers: false
 });
+const got = require("got");
+const config = require("../config.json");
+const snekfetch = require('snekfetch');
+const DBL = require("dblapi.js");
+const BOATS = require('boats.js');
+//const GBL = require('gblapi.js');
+const schedule = require('node-schedule');
+const { rewards } = require("../functions.js");
+
 const config = require("./config.json");
 
 const mongoose = require("mongoose");
@@ -14,6 +23,59 @@ mongoose.connect(config.mongoose_uri, {
     useUnifiedTopology: true
 });
 
+const dbl = new DBL(config.dbl_token, bot);
+
+dbl.postStats(botGuildCount).catch(e => console.log(e));
+
+
+const Boats = new BOATS(config.boatsToken);
+
+Boats.postStats(botGuildCount, "632291800585076761")
+    .catch((err) => {
+        console.log(err);
+    });
+
+/*const Glenn = new GBL(bot.user.id, config.glenToken, false, false);
+
+Glenn.updateStats(botGuildCount).catch(e => console.log(e));
+
+const updateBotList = async () => {
+
+const { body: reply } = await snekfetch.post(`https://discordbotlist.com/api/bots/632291800585076761/stats`)
+.set("Authorization", `Bot ${config.dblToken_2}`)
+.send({
+    guilds: botGuildCount,
+    users: bot.users.size,
+})
+
+return (reply);
+}
+
+let botUPDATE = await updateBotList();
+*/
+
+schedule.scheduleJob("1 12 * * 1", async function () {
+    const checkForWeeklyExecution = await Cooldown.findOne({ userID: bot.user.id, command: "weeklyrewards" });
+
+    if (checkForWeeklyExecution) return;
+    const weeklySpamStopper = new Cooldown({
+        command: "weeklyrewards",
+        userID: bot.user.id,
+        timeRemaining: Date.now() + 300000,
+        dateNow: Date.now()
+    });
+
+    await weeklySpamStopper.save().catch(err => console.log(err));
+    rewards(bot);
+});
+
+schedule.scheduleJob("0 9 * * *", async function () {
+    const checkForExecution = await Cooldown.findOne({ userID: bot.user.id, command: "giveaway" });
+
+    if (checkForExecution) return;
+    await bot.commands.get('giveaway').execute(bot);
+
+});
 
 bot.setMaxListeners(0);
 
