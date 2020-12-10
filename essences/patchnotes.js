854bba0d4bae545a355957645b97a23b69daa14f
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2020-12-10 10:26:11 +0100

    Update patchnotes.js

--- essences/patchnotes.js
+++ essences/patchnotes.js
@@ -1,7 +1,159 @@
 module.exports = {
-    PatchDate: "Dec.7th, 2020",
+    PatchDate: "Dec.10th, 2020",
     patchImage: "https://i.imgur.com/FuZtLio.png",
-    patchDateFooter: "Patch 12.07.20 - December 07, 2020",
+    patchDateFooter: "Patch 12.10.20 - December 10, 2020",
 
-    patchNotes: ``
+    patchNotes: `**Note:** This Patch were rewritten by a client, expect typos...
+        
+        Dear player:
+        Welcome to Identity V!
+        Identity V will undergo maintenance at 08:00 on December 10, 2020 (UTC+S) and will take
+        approximately 240 minutes to complete. During this time, players will be unable to log in to
+        servers. We ask for your understanding and patience during maintenance.
+        Maintenance content for this week:
+        Shop
+        
+        - Pet: All-new Survivor general A pet - Mini Bloody Queen is now available. It can be exchanged
+        using Fragments and Echoes.
+        
+        **Battle Adjustments**
+        
+        "Herald" Adjustments:
+        - Tier 0 Ability [Swoop!] Adjustment:
+        Added a new effect for tapping the ability the third time: Tapping for the third time enables the
+        Cat to expand the Paralysis Area and intimidate Survivors caught within. Intimidated Survivors
+        won't be able to decode, heal, or use items for 10 seconds. The Cat will return to the shadows after
+        the ability is cast, and the ability will enter cooldown.
+        
+        - Tier 0 Ability [Catwalk] Adjustment:
+        This ability is now rechargeable and can be recovered once every 16 seconds. In the process of
+        Dash Hit. the Dlauer may slightly adjust the direction by turning the camera.
+        
+        This ability will now increase the maximum rechargeable limit of Catwalk to 2.
+        Stat Adjustments:
+        
+        - Cat's outline is now visible within 40 meters instead of unlimited range.
+        New Mechanics:
+        
+        - Once the command to Intimidate is issued, the Cat will no longer be able to possess or confine
+        Survivors and is also unaffected bg the Command Mark.
+        
+        - Survivors who have been Intimidated will go stiff for 1 second, during which they can move but
+        certain actions will be interrupted.
+        
+        **Experience Optimizations**
+        
+        - When a Survivor is using "Euphoria, " a notification prompt will appear on their portrait, which
+        is only visible to Survivors (In Tarot mode, it is visible to players in the same faction).
+        
+        - Supplemented the description of Cowboy's external trait "Wild" and "Protective" against
+        Mechanic's doll.
+        
+        - Reduced the "Herald's" Deduction Difficultg in the Notebook (actual stats not affected).
+        
+        - Added an explanation in the Tarot Mode matching interface: If you match as the King or a
+        Squire, your role mag be switched.
+        
+        - "Undead" will no longer fall into the Moon River from performing Jump Chop.
+        
+        - Added a prompt when adding new friends.
+        
+        - When using the Magnet with the Prospector's Costume - Hajime Hinata, the character will
+        now be replaced with Izuru Kamukura's model. Players may choose to turn this effect on or off in
+        Special Effects Settings.
+        Improved the performance of the Mechanic's Costume - Futaba Sakura when playing the Emote
+        
+        - Exhausted.
+        
+        - Improved the performance of the Doctor's Costume - Mikan Tsumiki when playing the Emote -
+        Dance.
+        
+        - Slightly reduced the volume of some of the emotes of pet - Monomi.
+        
+        **Bug Fixes**
+        
+        - Fixed an issue in Tarot Mode where the original talent description was shown for Wanted Order, Symbiotic Effect, and Spectator while they had been adjusted.
+        
+        - Fixed an issue where the original description of Survivors' talents was shown while they had been adjusted due to 'Undead's" presence.
+        
+        - Fixed an issue where the Cowboy's Wild talent wouldn't activate when the Cowboy decoded with
+        the Mechanic's doll.
+        
+        - Fixed an issue where the Embalmer's coffin couldn't be transported if he used a new coffin
+        while the previous one was disappearing.
+        
+        - Fixed an issue where the Embalmer's item ability and intent icon didn't match.
+        
+        - Fixed an issue in Tarot Mode where players couldn't enter the correct character interface after
+        switching roles in the waiting lobby.
+        
+        - Fixed an issue where the location of the Priestess' Straight Passage was incorrect at a particular
+        location in Moon River Park.
+        
+        - Fixed an issue where Guard 26's bomb was erroneously consumed after entering Tier 2 with 6
+        bombs.
+        
+        - Fixed an issue where Coordinator's Costume "Dance in the Snow" was erroneous when using
+        Emotes "Lie Down" before using "Cheer.
+        
+        - Fixed an issue in Custom Tarot Mode where, for a slim chance, no Kings would be present in a
+        game.
+        
+        - Fixed an issue in the Tutorial where the Hell Ember and his Puppet would be teleported when
+        they swapped positions with each other.
+        
+        - Fixed an issue where the Geisha could send a Swallowtail Butterfly to a different Door in the
+        Golden Cave.
+        
+        - Fixed an issue where a Dream Witch's Follower would wander for a brief moment upon running
+        into a Cipher Machine on auto-pilot.
+        
+        - Fixed an issue where the ability of a Hunter who was immune to control effects would still be
+        canceled when they entered the effective range of a Drawing Board.
+        
+        - Fixed an issue where A1 could go through the cemetery's basement cover in Eversleeping Town.
+        
+        - Fixed an issue where the sound of a Cipher Machine that had been decoded couldn't be heard in
+        a game at times.
+        
+        - Fixed an issue where, when the "Undead" used Energy Conversion as his energy was decreasing,
+        his energy would continue to decrease before Energy Conversion took effect.
+        
+        - Fixed an issue where a Survivor couldn't heal themselves after being hit by the "Undead."
+        
+        - Fixed an issue where the "Undead" could end up outside the scene when he used Kinetic Dash
+        Hit toward an activated exit gate in the White Sand Street Asylum.
+        
+        - Fixed an issue where the attack backswing motion wasn't played after "Undead's" Jump Chop
+        hit a Survivor.
+        
+        - Fixed an issue where the knockdown elimination progress would not increase when "Borrowed
+        Time" was triggered at the same moment a Survivor was hit bg the "Undead."
+        
+        - Fixed an issue where the "Undead" would end up in a state where he could never be interrupted
+        after using his abilities under certain circumstances.
+        
+        - Fixed an issue where, when the Mechanic replaced the Mechanic's doll while carrying the
+        Accessory - Usami's Magic Wand, the models would overlap in the Mechanic's camera as the
+        Mechanic's doll went into a locker.
+        
+        - Fixed an issue in Memory Sphere Previous Seasons where the acquirement means of SIB items
+        were displayed incorrectly.
+        
+        Update Notice [December 17, 2020]
+        Identity V will be updated on December 17, 2020 (UTC+S). The following content is for reference
+        only, and the content of the actual maintenance notice shall prevail.
+        - New Arrival: To be, or not to be. The Embalmer Deduction Star Costume "Hamlet" is now
+        available.
+        
+        - The "Christmas Nocturne" warm-up event is now available: You stumble upon a violinist
+        blocking your path on Christmas Eve. He asks to plag you a song in exchange for some drinking
+        money. Before the music reaches a crescendo, the performance attracted even more spectators who
+        are going through a myriad of emotions as they're trying to seek comfort on Christmas Eve.
+        Complete the warm-up event to obtain an event themed portrait frame as a reward!
+        Thank you for your continued support for Identity V. We will attach gifts via in-game mail to
+        compensate for any inconvenience that the maintenance may ensue!
+        May the nightingale's singing follow you into our dream so you can find yourself again.
+        The traveler in your dream, Miss Nightingale.
+        `
 }
