2247c3af5eb22258f995d02cedda425491f448c0
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-01-06 17:07:27 +0100

    Update patchnotes.js

--- essences/patchnotes.js
+++ essences/patchnotes.js
@@ -16,81 +16,75 @@ module.exports = {
         
         Maintenance content for this week:
         
-        Events:
+        **Events:**
         
         - Snow Festival: Winter is here and the Snow Festival is upon us! Complete event quests to obtain
         Clues, Fragments, Costume Trial Cards, Snow Festival Portraits, and various rewards!
 
-        [Emote} Mercenary - Throws Snowball, [Emote] The Mind's Eye- Throws Snowball, [Emote]
-Magician - Throws Snowball, and [Emote] Perfumer- Throws Snowball will also be available in
-the Event Shop for a limited timet
+        [Emote] Mercenary - Throws Snowball, [Emote] The Mind's Eye- Throws Snowball, [Emote]
+        Magician - Throws Snowball, and [Emote] Perfumer- Throws Snowball will also be available in
+        the Event Shop for a limited timet
 
-Event Period: January 7, 2021 (after maintenance) - January 13, 2021 23:59:59 (Server Time)
-
- 
-
-~ Character Day: Freddy's (Lauyer) birthday (January 12) is here and the annual birthday party
-has officially begun. Participate inthe event and complete quests to get unique Character
-rewards!
+        Event Period: January 7, 2021 (after maintenance) - January 13, 2021 23:59:59 (Server Time)
 
  
-  
-   
-
-For Freddy's second birthday in Identity V, we've prepared various new rewards to celebrate this
-milestone! Players who have cbtained the portrait from his frst birthday event will receive a
-second-year Birthday Portrait Frame after completing the event quests! Those who haven't
-obtained the reward from Freddy's first birthday event wil recive his first-year birthday reward.
-
-Event Period: January 13, 00:00:00 - January 13, 23, 59, 59 (Server Time)
-
-Shop
-
-Package: (Batter Package - Donut) is now available for purchase at «limited time 20%
-discount when purchasing with Echoes (798 Echoes). The package contains the new Survivor
-character “Batter (Ganji Gupta)" and the Batter's B Costume “Donut.”
-
-Availability: January 7, 2021 (after maintenance) - January 20,2021, 23:59:59 (Server Time)
+        ~ Character Day: **Freddy's (Lawyer) birthday** (January 12) is here and the annual birthday party
+        has officially begun. Participate inthe event and complete quests to get unique Character
+        rewards!
 
  
+        For Freddy's second birthday in Identity V, we've prepared various new rewards to celebrate this
+        milestone! Players who have cbtained the portrait from his frst birthday event will receive a
+        second-year Birthday Portrait Frame after completing the event quests! Those who haven't
+        obtained the reward from Freddy's first birthday event wil recive his first-year birthday reward.
 
-~ Character: New Survivor "Batter"
-using 3,568 Clues or 688 Echoes.
+        Event Period: January 13, 00:00:00 - January 13, 23, 59, 59 (Server Time)
 
-‘now available in the shop. The Batter can be purchased
+        **Shop**
 
-- Costume: The Batter’s B Costume "Donut" is now available for purchase at 318 Echoes or 1188 fragments
+        Package: (Batter Package - Donut) is now available for purchase at limited time 20%
+        discount when purchasing with Echoes (798 Echoes). The package contains the new Survivor
+        character “Batter (Ganji Gupta)" and the Batter's B Costume “Donut”
 
-Bug fixes:
+        Availability: January 7, 2021 (after maintenance) - January 20,2021, 23:59:59 (Server Time)
 
-- Cortected the name of the Cowboy's Portrait "Monochrome Memory.”
+ 
+        ~ Character: New Survivor "Batter" now available in the shop.  The Batter can be purchased
+        using 3,568 Clues or 688 Echoes.
 
-Fixed an issue where the ballin the Emote "Ball Kicking” was missing when equipping the
-Gardener's Costume “Darjeeling.”
 
-- Fixed an issue in the Synesthesia Mode where Detectives could see the Photographer in the
-Camera World under certain circumstances.
+        - Costume: The Batter’s B Costume "Donut" is now available for purchase at 318 Echoes or 1188 fragments
 
-- Fixed an issue where the player who plays the Hunter would lose control oftheir character when
-the Enchantress’ Curse it the Hunter while landing from ajump.
+        **Bug fixes:**
 
-- Fixed an issue where the icon of the Barmaid's item would disappear under certain
-when reconnected to the server.
+        - Cortected the name of the Cowboy's Portrait "Monochrome Memory.”
 
-- Fixed an issue where snow piles were missing when using the Emote ~ Throws Snowball with
-some ofthe costumes of the Magician, the Perfumer, and the Mind's Eye.
+        Fixed an issue where the ballin the Emote "Ball Kicking” was missing when equipping the
+        Gardener's Costume “Darjeeling.”
 
-Update Notice January 14, 2021)
+        - Fixed an issue in the Synesthesia Mode where Detectives could see the Photographer in the
+        Camera World under certain circumstances.
 
-Identity V will be updated on January 14, 2021 (UTC+8). The following content is for reference
-only, and the content ofthe actual maintenance notice shall prevail.
+        - Fixed an issue where the player who plays the Hunter would lose control oftheir character when
+        the Enchantress’ Curse it the Hunter while landing from ajump.
 
-- Deduction Star Costume: The life of a Southern Belle is now gone with the wind. Bloody
-Queen's Deduction Star Costume, Scarlett, is now available in the shop and can be purchased
-with Fragments or Echoes.
+        - Fixed an issue where the icon of the Barmaid's item would disappear under certain
+        when reconnected to the server.
 
-Thank you for your continued support for Identity V. We will attack gifts via in game mail to.
-be compensate for any inconvenience thatthe maintenance may ensue!
+        - Fixed an issue where snow piles were missing when using the Emote ~ Throws Snowball with
+        some ofthe costumes of the Magician, the Perfumer, and the Mind's Eye.
+
+        **Update Notice (January 14, 2021)**
+
+        Identity V will be updated on January 14, 2021 (UTC+8). The following content is for reference
+        only, and the content ofthe actual maintenance notice shall prevail.
+
+        - Deduction Star Costume: The life of a Southern Belle is now gone with the wind. Bloody
+        Queen's Deduction Star Costume, Scarlett, is now available in the shop and can be purchased
+        with Fragments or Echoes.
+
+        Thank you for your continued support for Identity V. We will attack gifts via in game mail to.
+        be compensate for any inconvenience thatthe maintenance may ensue!
 
     `
 
