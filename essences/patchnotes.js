ea0181e87432eae8276a9fd9d5b0ad24972c967d
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-01-22 15:16:39 +0100

    Wrote the patchnotes :'D

--- essences/patchnotes.js
+++ essences/patchnotes.js
@@ -1,90 +1,178 @@
 const { stripIndents } = require('common-tags');
 
 module.exports = {
-    PatchDate: "Jan. 7th, 2021",
+    PatchDate: "Jan. 22th, 2021",
     patchImage: "https://i.imgur.com/FuZtLio.png",
-    patchDateFooter: "Patch 1.7.21 - January 7, 2021",
+    patchDateFooter: "Patch 1.21.21 - January 21, 2021",
 
     patchNotes:
         stripIndents`
-        Dear player:
         Welcome to Identity V!
-        
-        Identity V will undergo maintenance at 08:00 on January 7, 2021 (UTC+8) and will take
-        approximately 180 minutes to complete. During this time, players willbe unable to login to
-        servers. We ask for your understanding and patience during maintenance.
-        
-        Maintenance content for this week:
-        
-        **Events:**
-        
-        - Snow Festival: Winter is here and the Snow Festival is upon us! Complete event quests to obtain
-        Clues, Fragments, Costume Trial Cards, Snow Festival Portraits, and various rewards!
 
-        [Emote] Mercenary - Throws Snowball, [Emote] The Mind's Eye- Throws Snowball, [Emote]
-        Magician - Throws Snowball, and [Emote] Perfumer- Throws Snowball will also be available in
-        the Event Shop for a limited timet
+Identity V will undergo maintenance at 08:00 on January 21, 2021 (UTC+8) and will take
+approximately 240 minutes to complete. During this time, players will be unable to log in to
+servers, We ask for your understanding and patience during maintenance.
 
-        Event Period: January 7, 2021 (after maintenance) - January 13, 2021 23:59:59 (Server Time)
+Maintenance content for this week:
+Call of the Abyss:
 
+- [Abyss Treasure]: New Abyss Treasure has arrived along with a brand new lineup of costumes,
+including S Costume "Gamekeeper - Molten Hound", A Costumes "Prospector - Highway Cavalier",
+ "Barmaid - Fueling Agent,” "Priestess - Reflective Mirror", and "Acrobat - Pumping Tires."
+Players will not obtain duplicate A Costumes until all A Costumes have been obtained.
+
+
+**Event**
+
+- Character Day: Hastur's (Feaster) Character Day (January 24) is coming up along with the
+annual Hastur's Character Day Event! Participate in the event and complete Quests to get
+unique Character rewards!
+
+For Hastur's second Character Day in Identity V, we've prepared various new rewards to celebrate
+this milestone! Players who have obtained the portrait from his first Character Day event will
+receive a second-year Character Day Portrait Frame after completing the event quests! Those who
+haven't obtained the reward from Hastur's first Character Day event will receive his first-year
+Character Day reward.
+
+
+Event Period: January 24, 00:00;00 - January 24, 23:59:59 (Server Time)
+
+**Battle**
+
+- Once a Survivor who's tied to a balloon stopped struggling for 3 seconds, their struggle progress
+will increase by 3% per second (i.e., 50% of the normal struggling speed). This mechanism won't
+trigger any struggling movement, and it will halt when the Survivor begins to struggle and reset
+when they stop. However, this doesn't apply to a Survivor's Mirror Image captured by the
+Photographer in the Camera World.
+
+- Added the Batter's Cricket Hit Aim-Assist in Settings, which is turned off by default. Once
+it's turned on, the Batter will target the nearest Hunter when he uses the Cricket Bat.
+
+- The Batter can now see the outline of fallen Cricket Balls nearby.
+
+- The Mechanic's Doll will now be stunned when hit by the tram in Eversleeping Town.
+
+- The Postman won't receive the Bravery Letter and the Hope Letter at the beginning of a battle.
+- After exceeding a certain period of time for the Postman's Post Dog to deliver a letter, the Post
+Dog will appear in proximity to the recipient and deliver the letter directly if the recipient is
+available to accept delivery.
+
+**Shop**
+
+~ The Spyglass Package is available for a limited time at the cost of 288 Echoes. It contains 100
+Spyglasses, 188 Clues, and 188 Fragments.
+
+Availability: January 21, 2021 (after maintenance) - February 17, 2021, 23:59:59 (Server Time)
+
+**Experience Improvements**
+
+- Added pages available for the emotes/graffiti Wheel. Players will have to manually activate
+the second page at the Display Interface, which will give them 12 slots in the emotes/graffiti
+Wheel.
+
+- Improved the displaying rules of the emotes/graffiti Wheel so that players won't have to wait
+until the end of an emote before switching to another slot in the Wheel.
+
+- The maximum number of personal graffiti available in battle has been adjusted from 3 to 6.
+
+- Removed the "There are too many players queuing. Switch to another faction?" notification in
+Rank Matches.
+
+- If the matching failed due to unforeseeable circumstances (not being ready, reconnecting after
+disconnecting, losing connection, etc.), your waiting time will continue from the previous interruption if you restart matching within 1 minute.
+
+When Axe Boy uses a Resentful Soul to hit any target other than himself, feedback will be
+triggered from the hit on mobile devices.
+  
+
+- Added an introduction of the Synesthesia Mode in the Detective's costume selection interface.
+
+
+**Bug Fixes**
+- Fixed an issue where a Survivor would be able to struggle free immediately after they got
+captured by the Hunter when a Hunter was interrupted during the process of capturing a Survivor.
+
+- Fixed an issue on certain Android devices where the brightness of the Room was abnormal.
+
+- Fixed an issue in Synesthesia Mode where there's a chance that the Detective couldn't see the
+Hunter's Detention effect once they reconnected after losing connection.
+
+- Fixed an issue where the effects of the Seer's A Costume "Final Honor" could be seen from a
+distance.
+
+- Fixed an issue on some devices where the display on both sides of the screen on some devices was
+abnormal towards the end of a character diary's animation.
+
+- Fixed an issue where the destination of the Priestess's Straight Passage was placed incorrectly
+in some scenes.
+
+- Fixed an issue where the Al characters on the level below ground would behave abnormally when
+both of the Exit Gates in the Golden Cave map were opened.
+
+
+- Fixed an issue where the body double would be in the original state when Hajime Hinata used
+the Decoy in the Awaken state.
+
+- Fixed an issue where Hajime Hinata would end up in the original state when he transformed
+from the First Officer's image if the First Officer rescued Hajime Hinata in the Awaken state.
+
+- Fixed an issue where Hajime Hinata’s image on the Hunter's device would remain in the
+original state after Hajime Hinata used the Poseidon Watch in the Awaken state.
+
+- Fixed an issue where the Camera would behave abnormally when a Survivor used the Controller
+
+while using Gulliver's Travels to return to their original size and then switched the Camera back.
+
+abnormal in matches where the Dream Witch was present.
+
+- Fixed an issue where there's a chance that Survivor players' Deduction Points would be abnormal in matches where the Dream Witch was present.
+
+- Fixed an issue where the Sculptor's Sculptures, Feaster's Tentacles, and Guard 26's bombs
+couldn't deal damage to the Prospector for the duration of his Magnet's Attraction or Repulsion.
+
+- Fixed an issue where the Hunter could hit Survivors through scene objects at some locations in
+Moon River Park.
+
+Fixed an issue where the Priestess could use the Straight Passage to enter inaccessible areas at
+some locations in Moon River Park.
+
+- Fixed an issue where players who had obtained the Christmas costume couldn't view the
+corresponding Costume Trial Card.
+
+- Fixed an issue where the Tentacle in Feaster's A Costume "Exoplanet" was bigger than usual
+when holding the preview button.
+
+Fixed an issue where the Riding State icon wouldn't disappear after the Wildling escaped
+through an Exit Gate in Riding State.
+
+Fixed an issue where the Wildling Partner would sometimes get stuck in bumping state when
+the Wildling used Bump while the Photographer's Camera World was collapsing.
+
+Fixed an issue where the "bounce-back" animation was abnormal when the "Undead" was near a
+pallet.
+
+- Fixed an issue where, when the "Undead" used Charged Strike against the Wildling in Riding
+State and another Survivor at the same time, he would deal damage to the other Survivor but
+
+wouldn't enter attack recovery state.
+
+- Fixed an issue where the Soul Weaver's Standby Motion "Snooze" would overlap with certain costumes.
+
+- Fixed an issue where the incorrect COA icon was displayed in the Room.
+
+- Fixed an issue where the Results page failed to display the correct Snow Mountain Points after
+completing Abyss Battles.
  
-        ~ Character Day: **Freddy's (Lawyer) birthday** (January 12) is here and the annual birthday party
-        has officially begun. Participate inthe event and complete quests to get unique Character
-        rewards!
 
- 
-        For Freddy's second birthday in Identity V, we've prepared various new rewards to celebrate this
-        milestone! Players who have cbtained the portrait from his frst birthday event will receive a
-        second-year Birthday Portrait Frame after completing the event quests! Those who haven't
-        obtained the reward from Freddy's first birthday event wil recive his first-year birthday reward.
+- Fixed an issue where the entrance to Call of the Abyss would have display errors.
+Update Notice (January 28, 2021]
 
-        Event Period: January 13, 00:00:00 - January 13, 23, 59, 59 (Server Time)
+Identity V will be updated on January 28, 2021 (UTC+8). The following content is for reference
+only, and the content of the actual maintenance notice shall prevail. Be
 
-        **Shop**
+- The Ripper's S costume "Soul Emissary," Priestess’ $ costume "Morgan le Rey" and their corresponding accessories will make a limited time return! Stay tuned!
 
-        Package: (Batter Package - Donut) is now available for purchase at limited time 20%
-        discount when purchasing with Echoes (798 Echoes). The package contains the new Survivor
-        character “Batter (Ganji Gupta)" and the Batter's B Costume “Donut”
-
-        Availability: January 7, 2021 (after maintenance) - January 20,2021, 23:59:59 (Server Time)
-
- 
-        ~ Character: New Survivor "Batter" now available in the shop.  The Batter can be purchased
-        using 3,568 Clues or 688 Echoes.
-
-
-        - Costume: The Batter’s B Costume "Donut" is now available for purchase at 318 Echoes or 1188 fragments
-
-        **Bug fixes:**
-
-        - Cortected the name of the Cowboy's Portrait "Monochrome Memory.”
-
-        Fixed an issue where the ballin the Emote "Ball Kicking” was missing when equipping the
-        Gardener's Costume “Darjeeling.”
-
-        - Fixed an issue in the Synesthesia Mode where Detectives could see the Photographer in the
-        Camera World under certain circumstances.
-
-        - Fixed an issue where the player who plays the Hunter would lose control oftheir character when
-        the Enchantress’ Curse it the Hunter while landing from ajump.
-
-        - Fixed an issue where the icon of the Barmaid's item would disappear under certain
-        when reconnected to the server.
-
-        - Fixed an issue where snow piles were missing when using the Emote ~ Throws Snowball with
-        some ofthe costumes of the Magician, the Perfumer, and the Mind's Eye.
-
-        **Update Notice (January 14, 2021)**
-
-        Identity V will be updated on January 14, 2021 (UTC+8). The following content is for reference
-        only, and the content ofthe actual maintenance notice shall prevail.
-
-        - Deduction Star Costume: The life of a Southern Belle is now gone with the wind. Bloody
-        Queen's Deduction Star Costume, Scarlett, is now available in the shop and can be purchased
-        with Fragments or Echoes.
-
-        Thank you for your continued support for Identity V. We will attack gifts via in game mail to.
-        be compensate for any inconvenience thatthe maintenance may ensue!
+- New gamemode "Chasing shadows" is launching on January 28. Participate in the event to receive Dream Witch's Costume "Broadcaster" and various rewards!
 
     `
 
