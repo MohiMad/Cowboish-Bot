519ae191eb0184735b6c7dbd405491a6ecec4156
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-02-17 19:10:04 +0100

    >avatar finally workd

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -6,13 +6,19 @@ module.exports = {
     permissions: ["SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES"],
     category: "Utility",
     execute: async (message, args) => {
-        const u = await getMember(message, args.slice(1).join(" ")) || await getMember(message, message.author.id);
+
+        const u = await getMember(message, args.slice(1).join(" "));
+        if (!u) await getMember(message, message.author.id);
+
+        let formats = `[jpg](${u.user.avatarURL({ dynamic: false, size: 4096, format: 'jpg' })}) \`|\` [png](${u.user.avatarURL({ dynamic: false, size: 4096, format: 'png' })})`;
+        if (u.user.avatarURL({ dynamic: true, size: 4096 }).includes(".gif")) formats += `\`|\` [gif](${u.user.avatarURL({ dynamic: false, size: 4096, format: 'gif' })})`
 
         const avatar = new MessageEmbed()
             .setTitle(`${u.user.username}'s avatar!`)
-            .setDescription(`[jpg](${u.user.displayAvatarURL({ dynamic: true, format: 'jpg' })}) \`|\` [png](${u.user.displayAvatarURL({ dynamic: true, format: 'png' })})`)
-            .setColor(u.user.displayHexColor ? u.user.displayHexColor : "0xDFF42F")
-            .setImage(u.user.displayAvatarURL({ dynamic: true, size: 4096, format: 'jpeg' }));
+            .setDescription(formats)
+            .setColor(u.displayHexColor ? u.displayHexColor : "0xDFF42F")
+            .setImage(u.user.displayAvatarURL({ dynamic: true, size: 4096 }))
+            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }));
 
         return message.channel.send(avatar).catch(e => console.log(e));
     }
