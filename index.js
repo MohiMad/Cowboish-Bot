13103c3762a6f0240af41dabe08966148583cd25
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-03-25 20:28:33 +0100

    Updating version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { ShardingManager } = require('discord.js');
+
 const manager = new ShardingManager('./bot.js', { 
     token: process.env.token 
 });
 
 manager.on('shardCreate', (shard) => {
-    console.log(`Launched shard ${shard.id}`)
+    console.log(`Launched shard ${shard.id}`);
 });
 
 manager.spawn();
