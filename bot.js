5f22c028b19aa54bb021bd7213b6cd27c30209a8
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-03-25 20:14:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client({
     messageCacheMaxSize: 35,
-    shardCount: 'auto'
+    shards: 'auto'
 });
 
 const schedule = require('node-schedule');
