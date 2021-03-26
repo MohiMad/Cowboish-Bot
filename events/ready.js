aeb03130e9133ff337517799e53fb2a80bb7d723
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-03-26 19:46:54 +0100

    x

--- events/ready.js
+++ events/ready.js
@@ -9,6 +9,10 @@ const { getUsersCount, getServerCount } = require("../src/functions.js");
 
 module.exports = async (bot) => {
 
+    setTimeout(() => {
+        //boot time
+    }, 10000);
+
     const botGuildCount = await getServerCount(bot);
     const userCount = await getUsersCount(bot);
 
@@ -20,10 +24,10 @@ module.exports = async (bot) => {
     const activities_list = [
         `and yoinking around >:v`,
         `>invite | >help`,
-        `Identity V in ${botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0)} servers 💕`,
-        `With ${userCount.reduce((acc, memberCount) => acc + memberCount, 0)} damsels ;)`,
+        `Identity V in ${botGuildCount} servers 💕`,
+        `With ${userCount} damsels ;)`,
         "Welcome to Identit.",
-        `Milestone ${botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0)}/2500`,
+        `Milestone ${botGuildCount}/2500`,
         `Never forget Bonbon's "铁皮人" skin`,
         `Someone pay NetEase an English translator`,
         `I'm not forgiving NE for naming my Black-and-White Portrait "Cowgirl"`,
@@ -36,90 +40,91 @@ module.exports = async (bot) => {
         bot.user.setActivity(activities_list[index]);
     }, 300000);
 
-    /* 
-    
-        setInterval(async () => {
-    
-            const cooldownCheck = await Cooldown.find({});
-    
-            for (const cooldown of cooldownCheck) {
-    
-                const timeLeft = new Date(cooldown.timeRemaining);
-    
-                let check = timeLeft - Date.now() >= timeLeft || timeLeft - Date.now() <= 0;
-    
-                if (check) {
-                    if (!cooldownCheck) return;
-    
-                    await cooldown.delete({}).catch(e => console.log(e));
-                }
-    
+    /
+
+    setInterval(async () => {
+
+        const cooldownCheck = await Cooldown.find({});
+
+        for (const cooldown of cooldownCheck) {
+
+            const timeLeft = new Date(cooldown.timeRemaining);
+
+            let check = timeLeft - Date.now() >= timeLeft || timeLeft - Date.now() <= 0;
+
+            if (check) {
+                if (!cooldownCheck) return;
+
+                await cooldown.delete({}).catch(e => console.log(e));
             }
-    
-            const mutes = await Mutes.find({});
-    
-            if (!mutes) return;
-            for (const mute of mutes) {
-                async function deleteDoc() {
-                    await Mutes.deleteOne(mute).catch(e => console.log(e));
-                }
-    
-                if (mute.created + mute.muteTime <= Date.now() || mute.muteTime < 1000) {
-                    const guild = await bot.shard.broadcastEval(`this.guilds.cache.get("${mute.guildID}")`);
-                    if (!guild) return deleteDoc();
-    
-                    const member = await guild.members.cache.get(mute.userID);
-    
-                    if (!member) return deleteDoc();
-    
-                    let muteRole = await guild.roles.cache.find((x) => x.name === "muted");
-    
-                    if (!muteRole) muteRole = await guild.roles.create({ name: "muted", color: "#27272b", permissions: [] });
-    
-                    if (!member.roles.cache.has(muteRole.id)) return deleteDoc();
-    
-                    await member.roles.remove(muteRole);
-    
-                    const logChannel = await guild.channels.cache.get(mute.channelID);
-    
-                    if (!logChannel) return deleteDoc();
-    
-                    logChannel.send(`${member.user} has been unmuted!`);
-                    deleteDoc();
-    
-                }
-            }
-        }, 2000);
-    
-    
-        const dbl = new DBL(process.env.dbl_token, bot);
-    
-        dbl.postStats(botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0)).catch(e => console.log(e));
-    
-    
-        const Boats = new BOATS(process.env.boatsToken);
-    
-        Boats.postStats(botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0), "632291800585076761")
-            .catch((err) => console.log(err));
-    
-        /*const Glenn = new GBL(bot.user.id, process.env.glenToken, false, false);
-        
-        Glenn.updateStats(botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0)).catch(e => console.log(e));
-        
-        const updateBotList = async () => {
-        
-        const { body: reply } = await snekfetch.post(`https://discordbotlist.com/api/bots/632291800585076761/stats`)
-        .set("Authorization", `Bot ${process.env.dblToken_2}`)
-        .send({
-            guilds: botGuildCount.reduce((acc, guildCount) => acc + guildCount, 0),
-            users: bot.users.size,
-        })
-        
-        return (reply);
+
         }
-        
-        let botUPDATE = await updateBotList();
-        */
+
+        const mutes = await Mutes.find({});
+
+        if (!mutes) return;
+        for (const mute of mutes) {
+            async function deleteDoc() {
+                await Mutes.deleteOne(mute).catch(e => console.log(e));
+            }
+
+            if (mute.created + mute.muteTime <= Date.now() || mute.muteTime < 1000) {
+                const guild = await bot.shard.broadcastEval(`this.guilds.cache.get("${mute.guildID}")`);
+                if (!guild) return deleteDoc();
+
+                const member = await guild.members.cache.get(mute.userID);
+
+                if (!member) return deleteDoc();
+
+                let muteRole = await guild.roles.cache.find((x) => x.name === "muted");
+
+                if (!muteRole) muteRole = await guild.roles.create({ name: "muted", color: "#27272b", permissions: [] });
+
+                if (!member.roles.cache.has(muteRole.id)) return deleteDoc();
+
+                await member.roles.remove(muteRole);
+
+                const logChannel = await guild.channels.cache.get(mute.channelID);
+
+                if (!logChannel) return deleteDoc();
+
+                logChannel.send(`${member.user} has been unmuted!`);
+                deleteDoc();
+
+            }
+        }
+    }, 2000);
+
+
+    const dbl = new DBL(process.env.dbl_token, bot);
+
+    dbl.postStats(botGuildCount).catch(e => console.log(e));
+
+
+    const Boats = new BOATS(process.env.boatsToken);
+
+    Boats.postStats(botGuildCount, "632291800585076761")
+        .catch((err) => console.log(err));
+
+
+    /*const Glenn = new GBL(bot.user.id, process.env.glenToken, false, false);
+    
+    Glenn.updateStats(botGuildCount).catch(e => console.log(e));
+    
+    const updateBotList = async () => {
+    
+    const { body: reply } = await snekfetch.post(`https://discordbotlist.com/api/bots/632291800585076761/stats`)
+    .set("Authorization", `Bot ${process.env.dblToken_2}`)
+    .send({
+        guilds: botGuildCount,
+        users: bot.users.size,
+    })
+    
+    return (reply);
+    }
+    
+    let botUPDATE = await updateBotList();
+    */
 
 
 };
