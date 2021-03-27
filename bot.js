57257a39d45110aecf86027b3988d89ab8240f12
MohiMoo <56826704+MohiMad@users.noreply.github.com>
2021-03-27 16:52:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,14 +8,15 @@ const mongoose = require("mongoose");
 
 
 bot.on('shardReady', async (shard) => {
-    console.log(shard, bot.shard.count);
-    /* mongoose.connect(process.env.mongoose_uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    if ((shard + 1) === bot.shard.count) {
+        mongoose.connect(process.env.mongoose_uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-    ["event", "command"].forEach(x => require(`./handlers/${x}`)(bot)); */
-})
+        ["event", "command"].forEach(x => require(`./handlers/${x}`)(bot));
+    }
+});
 
 bot.setMaxListeners(0);
 
